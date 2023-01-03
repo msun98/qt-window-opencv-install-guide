71d82bfcf3e1b4e7135d1e611ec0a92cef3b8c28
MS_KIM <46109587+msun98@users.noreply.github.com>
2023-01-03 09:10:12 +0900

    Add files via upload

---
+++ include/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Shoes/Documents/common/opencv/sources/include
+# Build directory: C:/Users/Shoes/Documents/common/release/include
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
